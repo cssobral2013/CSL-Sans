084e4dc62bd304999568b992f0625e8bc066d162
Cristiano Sobral <cssobral2013@gmail.com>
2024-04-23 20:44:31 -0300

    Update v4.0 (re-base)

---
+++ generate-woff-woff2.py
@@ -0,0 +1,301 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/CSLSans-Thin.ttf'
+woffPath = './web/CSLSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CSLSans-ThinItalic.ttf'
+woffPath = './web/CSLSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CSLSans-ExtraLight.ttf'
+woffPath = './web/CSLSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CSLSans-ExtraLightItalic.ttf'
+woffPath = './web/CSLSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CSLSans-Light.ttf'
+woffPath = './web/CSLSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-LightItalic.ttf'
+woffPath = './web/CSLSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-LightItalic.ttf'
+woffPath = './web/CSLSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-Regular.ttf'
+woffPath = './web/CSLSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-Italic.ttf'
+woffPath = './web/CSLSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-Medium.ttf'
+woffPath = './web/CSLSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-MediumItalic.ttf'
+woffPath = './web/CSLSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBold.ttf'
+woffPath = './web/CSLSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBoldItalic.ttf'
+woffPath = './web/CSLSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBold.ttf'
+woffPath = './web/CSLSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBoldItalic.ttf'
+woffPath = './web/CSLSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-Bold.ttf'
+woffPath = './web/CSLSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-BoldItalic.ttf'
+woffPath = './web/CSLSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-ExtraBold.ttf'
+woffPath = './web/CSLSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CSLSans-ExtraBoldItalic.ttf'
+woffPath = './web/CSLSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/CSLSans-Thin.ttf'
+woff2Path = './web/CSLSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CSLSans-ThinItalic.ttf'
+woff2Path = './web/CSLSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CSLSans-ExtraLight.ttf'
+woff2Path = './web/CSLSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CSLSans-ExtraLightItalic.ttf'
+woff2Path = './web/CSLSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/CSLSans-Light.ttf'
+woff2Path = './web/CSLSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-LightItalic.ttf'
+woff2Path = './web/CSLSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-LightItalic.ttf'
+woff2Path = './web/CSLSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-Regular.ttf'
+woff2Path = './web/CSLSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-Italic.ttf'
+woff2Path = './web/CSLSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-Medium.ttf'
+woff2Path = './web/CSLSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-MediumItalic.ttf'
+woff2Path = './web/CSLSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBold.ttf'
+woff2Path = './web/CSLSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBoldItalic.ttf'
+woff2Path = './web/CSLSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBold.ttf'
+woff2Path = './web/CSLSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-SemiBoldItalic.ttf'
+woff2Path = './web/CSLSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-Bold.ttf'
+woff2Path = './web/CSLSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-BoldItalic.ttf'
+woff2Path = './web/CSLSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-ExtraBold.ttf'
+woff2Path = './web/CSLSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CSLSans-ExtraBoldItalic.ttf'
+woff2Path = './web/CSLSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
